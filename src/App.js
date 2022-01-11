32deda8163a70593aa06d1f52491475429b7e69e
nedosika <nedosika@gmail.com>
2022-01-11 16:18:26 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Form from "./components/Form";
+import Field from "./components/Field";
+import Select from "./components/Select";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const handleSubmit = () => {
+        console.log('submit')
+    }
+    return (
+        <Form onSubmit={handleSubmit}>
+            <Field
+                type="text"
+                name='firstName'
+                label='First Name:'
+                initialValue='sss'
+            />
+            <Select name='color'>
+                <option value="red">Red</option>
+                <option value="green">Green</option>
+                <option value="blue">Blue</option>
+            </Select>
+            <button type="submit">Submit</button>
+        </Form>
+    );
 }
 
 export default App;
